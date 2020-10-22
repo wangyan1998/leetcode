6af4d92ab61ec68074a9c75dd168446d17854019
wangyan1998 <1011344314@qq.com>
2020-10-22 16:07:13 +0800

    Initial commit

---
+++ src/toolclass/ListNode.java
@@ -0,0 +1,9 @@
+package toolclass;
+
+public class ListNode {
+    public ListNode next;
+    public int val;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
