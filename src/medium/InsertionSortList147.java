5d468513170d8ecbeccc8781be310bf6359353e3
wangyan1998 <1011344314@qq.com>
2020-11-20 09:59:39 +0800

    LeetCode

---
+++ src/medium/InsertionSortList147.java
@@ -0,0 +1,35 @@
+package medium;
+//对链表进行插入排序。
+//        插入排序算法：
+//        插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+//        每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+//        重复直到所有输入数据插入完为止。
+
+import toolclass.ListNode;
+
+public class InsertionSortList147 {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode lastSorted = head, curr = head.next;
+        while (curr != null) {
+            if (lastSorted.val <= curr.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode prev = dummyHead;
+                while (prev.next.val <= curr.val) {
+                    prev = prev.next;
+                }
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+        return dummyHead.next;
+
+    }
+}
--- src/toolclass/ListNode.java
+++ src/toolclass/ListNode.java
@@ -4,6 +4,6 @@ public class ListNode {
     public ListNode next;
     public int val;
     ListNode() {}
-    ListNode(int val) { this.val = val; }
+    public ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
