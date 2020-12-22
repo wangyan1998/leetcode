7dbcc3c039d7ee055a38a18c532e6c9a60cef7eb
wangyan1998 <1011344314@qq.com>
2020-12-22 20:45:46 +0800

    algorithm

---
+++ src/classify/sort/RadixSort.java
@@ -0,0 +1,45 @@
+package classify.sort;
+
+import java.util.ArrayList;
+
+public class RadixSort {
+    /**
+     * 基数排序
+     * @param array
+     * @return
+     * 取得数组中的最大数，并取得位数；
+     * arr为原始数组，从最低位开始取每个位组成radix数组；
+     * 对radix进行计数排序（利用计数排序适用于小范围数的特点）；
+     */
+    public int[] RadixSort(int[] array) {
+        if (array == null || array.length < 2)
+            return array;
+        // 1.先算出最大数的位数；
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            max = Math.max(max, array[i]);
+        }
+        int maxDigit = 0;
+        while (max != 0) {
+            max /= 10;
+            maxDigit++;
+        }
+        int mod = 10, div = 1;
+        ArrayList<ArrayList<Integer>> bucketList = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < 10; i++)
+            bucketList.add(new ArrayList<Integer>());
+        for (int i = 0; i < maxDigit; i++, mod *= 10, div *= 10) {
+            for (int j = 0; j < array.length; j++) {
+                int num = (array[j] % mod) / div;
+                bucketList.get(num).add(array[j]);
+            }
+            int index = 0;
+            for (int j = 0; j < bucketList.size(); j++) {
+                for (int k = 0; k < bucketList.get(j).size(); k++)
+                    array[index++] = bucketList.get(j).get(k);
+                bucketList.get(j).clear();
+            }
+        }
+        return array;
+    }
+}
