f2b5d7b40d084d19792257ccda323908435a97f8
wangyan1998 <1011344314@qq.com>
2021-02-10 13:52:31 +0800

    algorithm

--- src/medium/CheckInclusion567.java
+++ src/medium/CheckInclusion567.java
@@ -6,6 +6,12 @@ import java.util.Set;
 //给定两个字符串 s1 和 s2，写一个函数来判断 s2 是否包含 s1 的排列。
 //        换句话说，第一个字符串的排列之一是第二个字符串的子串。
 public class CheckInclusion567 {
+    /**
+     * 滑动窗口
+     * @param s1
+     * @param s2
+     * @return
+     */
     public boolean checkInclusion(String s1,String s2){
         int n = s1.length(), m = s2.length();
         if (n > m) {
@@ -24,6 +30,36 @@ public class CheckInclusion567 {
             ++cnt2[s2.charAt(i) - 'a'];
             --cnt2[s2.charAt(i - n) - 'a'];
             if (Arrays.equals(cnt1, cnt2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 双指针方法
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public boolean checkInclusion1(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        if (n > m) {
+            return false;
+        }
+        int[] cnt = new int[26];
+        for (int i = 0; i < n; ++i) {
+            --cnt[s1.charAt(i) - 'a'];
+        }
+        int left = 0;
+        for (int right = 0; right < m; ++right) {
+            int x = s2.charAt(right) - 'a';
+            ++cnt[x];
+            while (cnt[x] > 0) {
+                --cnt[s2.charAt(left) - 'a'];
+                ++left;
+            }
+            if (right - left + 1 == n) {
                 return true;
             }
         }
