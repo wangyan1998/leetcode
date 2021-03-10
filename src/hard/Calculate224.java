c87527ec60bc0f096ff4129ba9d895baaaeda21c
wangyan1998 <1011344314@qq.com>
2021-03-10 17:32:43 +0800

    algorithm

---
+++ src/hard/Calculate224.java
@@ -0,0 +1,44 @@
+package hard;
+//实现一个基本的计算器来计算一个简单的字符串表达式 s 的值。
+//        1 <= s.length <= 3 * 10^5
+//        s 由数字、'+'、'-'、'('、')'、和 ' ' 组成
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Calculate224 {
+    public int calculate(String s){
+        Deque<Integer> ops = new LinkedList<Integer>();
+        ops.push(1);
+        int sign = 1;
+
+        int ret = 0;
+        int n = s.length();
+        int i = 0;
+        while (i < n) {
+            if (s.charAt(i) == ' ') {
+                i++;
+            } else if (s.charAt(i) == '+') {
+                sign = ops.peek();
+                i++;
+            } else if (s.charAt(i) == '-') {
+                sign = -ops.peek();
+                i++;
+            } else if (s.charAt(i) == '(') {
+                ops.push(sign);
+                i++;
+            } else if (s.charAt(i) == ')') {
+                ops.pop();
+                i++;
+            } else {
+                long num = 0;
+                while (i < n && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + s.charAt(i) - '0';
+                    i++;
+                }
+                ret += sign * num;
+            }
+        }
+        return ret;
+    }
+}
