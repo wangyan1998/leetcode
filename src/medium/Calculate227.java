9a0582a86524d19cbc3ce40fb8685c6d6cf74904
wangyan1998 <1011344314@qq.com>
2021-03-11 10:15:03 +0800

    algorithm

---
+++ src/medium/Calculate227.java
@@ -0,0 +1,41 @@
+package medium;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Calculate227 {
+    public int calculate(String s){
+        char[] cs = s.trim().toCharArray();
+        Deque<Integer> st = new ArrayDeque<Integer>();
+        int ans = 0, i = 0;
+        while(i < cs.length){
+            if(cs[i] == ' ') {i++;continue;}
+            char tmp = cs[i];
+            if(tmp == '*' || tmp == '/' || tmp == '+' || tmp == '-'){
+                i++;
+                while(i < cs.length && cs[i] == ' ') i++;
+            }
+            int num = 0;
+            while(i < cs.length && Character.isDigit(cs[i])){
+                num = num * 10 + cs[i] - '0';
+                i++;
+            }
+            switch(tmp){
+                case '-':
+                    num = -num;
+                    break;
+                case '*':
+                    num = st.pop() * num;
+                    break;
+                case '/':
+                    num = st.pop() / num;
+                    break;
+                default:
+                    break;
+            }
+            st.push(num);
+        }
+        while(!st.isEmpty()) ans += st.pop();
+        return ans;
+    }
+}
