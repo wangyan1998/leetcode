19ed2b01396b852f316779d6d94217d0290e3857
wangyan1998 <1011344314@qq.com>
2021-03-15 11:30:56 +0800

    algorithm

---
+++ src/medium/SpiralOrder54.java
@@ -0,0 +1,45 @@
+package medium;
+//给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SpiralOrder54 {
+    /**
+     * 边界法，设定四边的边界，每次遍历都改变边界，转着圈的进行遍历
+     * @param matrix
+     * @return
+     */
+    public List<Integer> spiralOrder(int[][] matrix){
+        LinkedList<Integer> result = new LinkedList<>();
+        if(matrix==null||matrix.length==0) return result;
+        int left = 0;
+        int right = matrix[0].length - 1;
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int numEle = matrix.length * matrix[0].length;
+        while (numEle >= 1) {
+            for (int i = left; i <= right && numEle >= 1; i++) {
+                result.add(matrix[top][i]);
+                numEle--;
+            }
+            top++;
+            for (int i = top; i <= bottom && numEle >= 1; i++) {
+                result.add(matrix[i][right]);
+                numEle--;
+            }
+            right--;
+            for (int i = right; i >= left && numEle >= 1; i--) {
+                result.add(matrix[bottom][i]);
+                numEle--;
+            }
+            bottom--;
+            for (int i = bottom; i >= top && numEle >= 1; i--) {
+                result.add(matrix[i][left]);
+                numEle--;
+            }
+            left++;
+        }
+        return result;
+    }
+}
