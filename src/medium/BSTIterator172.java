0a14bf7d10327eb0fb290b279af5082b2265e96c
wangyan1998 <1011344314@qq.com>
2021-03-28 10:05:42 +0800

    algorithm

---
+++ src/medium/BSTIterator172.java
@@ -0,0 +1,45 @@
+package medium;
+//实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+//        BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root 会作为构造函数的一部分给出。
+//        指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
+//        boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
+//        int next()将指针向右移动，然后返回指针处的数字。
+//        注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
+//        你可以假设next()调用总是有效的，也就是说，当调用 next()时，BST 的中序遍历中至少存在一个下一个数字。
+
+
+import toolclass.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+/**
+ * @author wy
+ * @date 2021/3/28 9:36
+ */
+public class BSTIterator172 {
+    private int idx;
+    private List<Integer> arr;
+    public BSTIterator172(TreeNode root) {
+        idx = 0;
+        arr = new ArrayList<Integer>();
+        inorderTraversal(root, arr);
+    }
+
+    public int next() {
+       return arr.get(idx++);
+    }
+
+    public boolean hasNext() {
+       return idx<arr.size();
+    }
+    private void inorderTraversal(TreeNode root, List<Integer> arr) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, arr);
+        arr.add(root.val);
+        inorderTraversal(root.right, arr);
+    }
+}
