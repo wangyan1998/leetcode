148db5e08880054f1466a556dc245de0ff7c22d2
wangyan1998 <1011344314@qq.com>
2021-03-29 09:53:56 +0800

    algorithm

---
+++ src/classify/test/testadd.java
@@ -0,0 +1,14 @@
+package classify.test;
+
+/**
+ * @author wy
+ * @date 2021/3/28 10:19
+ */
+public class testadd {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
