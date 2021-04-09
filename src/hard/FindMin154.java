39059d979faa969580ac09b8f0243d28fc5ccfaa
wangyan1998 <1011344314@qq.com>
2021-04-09 09:10:54 +0800

    algorithm

---
+++ src/hard/FindMin154.java
@@ -0,0 +1,49 @@
+package hard;
+//已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。
+//        例如，原数组 nums = [0,1,4,4,5,6,7] 在变化后可能得到：
+//        若旋转 4 次，则可以得到 [4,5,6,7,0,1,4]
+//        若旋转 7 次，则可以得到 [0,1,4,4,5,6,7]
+//        注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+//        给你一个可能存在 重复 元素值的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。
+//        请你找出并返回数组中的 最小元素 。
+
+
+/**
+ * @author wy
+ * @date 2021/4/9 8:32
+ */
+public class FindMin154 {
+    public int findMin(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int j = nums[0];
+        if (j < nums[nums.length - 1]) {
+            return j;
+        } else {
+            int i = nums.length - 1;
+            for (; i > 0; i--) {
+                if (nums[i - 1] > nums[i]) {
+                    break;
+                }
+            }
+            return nums[i];
+        }
+    }
+
+    public int findMin1(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (nums[pivot] < nums[high]) {
+                high = pivot;
+            } else if (nums[pivot] > nums[high]) {
+                low = pivot + 1;
+            } else {
+                high -= 1;
+            }
+        }
+        return nums[low];
+    }
+}
