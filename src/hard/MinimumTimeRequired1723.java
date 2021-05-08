44e797a68276c23c519d7b9a8ed501e4bd480a33
wangyan1998 <1011344314@qq.com>
2021-05-08 10:16:10 +0800

    algorithm

---
+++ src/hard/MinimumTimeRequired1723.java
@@ -0,0 +1,102 @@
+package hard;
+//给你一个整数数组 jobs ，其中 jobs[i] 是完成第 i 项工作要花费的时间。
+//        请你将这些工作分配给 k 位工人。所有工作都应该分配给工人，且每项工作只能分配给一位工人。
+//        工人的 工作时间 是完成分配给他们的所有工作花费时间的总和。请你设计一套最佳的工作分配方案，
+//        使工人的 最大工作时间得以最小化 。
+//        返回分配方案中尽可能 最小 的 最大工作时间 。
+
+
+import java.util.Arrays;
+
+/**
+ * @author wy
+ * @date 2021/5/8 9:33
+ */
+public class MinimumTimeRequired1723 {
+    /**
+     * 动态规划+状态压缩
+     * @param jobs
+     * @param k
+     * @return
+     */
+    public int minimumTimeRequired(int[] jobs, int k) {
+        int n = jobs.length;
+        int[] sum = new int[1 << n];
+        for (int i = 1; i < (1 << n); i++) {
+            int x = Integer.numberOfTrailingZeros(i), y = i - (1 << x);
+            sum[i] = sum[y] + jobs[x];
+        }
+
+        int[][] dp = new int[k][1 << n];
+        for (int i = 0; i < (1 << n); i++) {
+            dp[0][i] = sum[i];
+        }
+
+        for (int i = 1; i < k; i++) {
+            for (int j = 0; j < (1 << n); j++) {
+                int minn = Integer.MAX_VALUE;
+                for (int x = j; x != 0; x = (x - 1) & j) {
+                    minn = Math.min(minn, Math.max(dp[i - 1][j - x], sum[x]));
+                }
+                dp[i][j] = minn;
+            }
+        }
+        return dp[k - 1][(1 << n) - 1];
+    }
+
+    /**
+     * 二分查找+回溯+剪枝
+     * @param jobs
+     * @param k
+     * @return
+     */
+    public int minimumTimeRequired1(int[] jobs, int k) {
+        Arrays.sort(jobs);
+        int low = 0, high = jobs.length - 1;
+        while (low < high) {
+            int temp = jobs[low];
+            jobs[low] = jobs[high];
+            jobs[high] = temp;
+            low++;
+            high--;
+        }
+        int l = jobs[0], r = Arrays.stream(jobs).sum();
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (check(jobs, k, mid)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+    public boolean check(int[] jobs, int k, int limit) {
+        int[] workloads = new int[k];
+        return backtrack(jobs, workloads, 0, limit);
+    }
+
+    public boolean backtrack(int[] jobs, int[] workloads, int i, int limit) {
+        if (i >= jobs.length) {
+            return true;
+        }
+        int cur = jobs[i];
+        for (int j = 0; j < workloads.length; ++j) {
+            if (workloads[j] + cur <= limit) {
+                workloads[j] += cur;
+                if (backtrack(jobs, workloads, i + 1, limit)) {
+                    return true;
+                }
+                workloads[j] -= cur;
+            }
+            // 如果当前工人未被分配工作，那么下一个工人也必然未被分配工作
+            // 或者当前工作恰能使该工人的工作量达到了上限
+            // 这两种情况下我们无需尝试继续分配工作
+            if (workloads[j] == 0 || workloads[j] + cur == limit) {
+                break;
+            }
+        }
+        return false;
+    }
+}
