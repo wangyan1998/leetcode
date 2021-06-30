a5498628fd547d9149989fce01c5dcb160cfbdd2
wangyan1998 <1011344314@qq.com>
2021-06-30 09:56:41 +0800

    algorithm

---
+++ src/hard/CodecJZ37.java
@@ -0,0 +1,50 @@
+package hard;
+//请实现两个函数，分别用来序列化和反序列化二叉树。
+//        你需要设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，
+//        你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+
+
+import toolclass.TreeNode;
+
+import java.util.*;
+
+/**
+ * @author wy
+ * @date 2021/6/30 8:50
+ */
+public class CodecJZ37 {
+    public String serialize(TreeNode root) {
+        return rserialize(root, "");
+    }
+
+    public TreeNode deserialize(String data) {
+        String[] dataArray = data.split(",");
+        List<String> dataList = new LinkedList<String>(Arrays.asList(dataArray));
+        return rdeserialize(dataList);
+    }
+
+    public String rserialize(TreeNode root, String str) {
+        if (root == null) {
+            str += "None,";
+        } else {
+            str += str.valueOf(root.val) + ",";
+            str = rserialize(root.left, str);
+            str = rserialize(root.right, str);
+        }
+        return str;
+    }
+
+    public TreeNode rdeserialize(List<String> dataList) {
+        if (dataList.get(0).equals("None")) {
+            dataList.remove(0);
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.valueOf(dataList.get(0)));
+        dataList.remove(0);
+        root.left = rdeserialize(dataList);
+        root.right = rdeserialize(dataList);
+
+        return root;
+    }
+}
