893e058ca338aca6c53af8a876a31d255feb85f5
wangyan1998 <1011344314@qq.com>
2021-08-31 10:09:57 +0800

    algorithm

--- src/medium/CorpFlightBooking1109.java
+++ src/medium/CorpFlightBooking1109.java
@@ -24,4 +24,24 @@ public class CorpFlightBooking1109 {
         }
         return res;
     }
+
+    /**
+     * 差分数组
+     * @param bookings
+     * @param n
+     * @return
+     */
+    public int[] corpFlightBookings1(int[][] bookings, int n) {
+        int[] nums = new int[n];
+        for (int[] booking : bookings) {
+            nums[booking[0] - 1] += booking[2];
+            if (booking[1] < n) {
+                nums[booking[1]] -= booking[2];
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
 }
