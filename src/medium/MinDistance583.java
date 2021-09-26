57e5a245b7afb2b3f6ff9830b55fd984641b4673
wangyan1998 <1011344314@qq.com>
2021-09-26 10:22:11 +0800

    algorithm

---
+++ src/medium/MinDistance583.java
@@ -0,0 +1,27 @@
+package medium;
+//给定两个单词 word1 和 word2，找到使得 word1 和 word2 相同所需的最小步数，每步可以删除任意一个字符串中的一个字符。
+
+/**
+ * @author wy
+ * @date 2021/9/25 8:31
+ */
+public class MinDistance583 {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            char c1 = word1.charAt(i - 1);
+            for (int j = 1; j <= n; j++) {
+                char c2 = word2.charAt(j - 1);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        int lcs = dp[m][n];
+        return m - lcs + n - lcs;
+    }
+}
